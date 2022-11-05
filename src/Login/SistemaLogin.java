102b7da08d1c06362d4b950b2778e741a68e5729
AndersonJader <andersonjader036@gmail.com>
2022-11-05 18:23:46 -0300

    New updates

--- src/Login/SistemaLogin.java
+++ src/Login/SistemaLogin.java
@@ -2,11 +2,15 @@ package src.Login;
 
 import src.Loja.SystemShop;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 import static src.Application.Main.operations;
+import static src.Loja.SystemShop.shopInterface;
 
 public class SistemaLogin {
+
+    private static char genero;
     private static int senhaAdm = 12;
     static Scanner keyboard = new Scanner(System.in);
 
@@ -22,25 +26,76 @@ public class SistemaLogin {
         if (opcao == 1) {
             System.out.println("Digite a senha: ");
             int login = keyboard.nextInt();
+
             if (login == senhaAdm) {
                 operations();
             } else {
                 Usuario.login(login);
             }
         } else if (opcao == 2) {
-            System.out.println("-------Cadastro------");
-            System.out.println("Senha: ");
-            int senhaCadastro = keyboard.nextInt();
-            System.out.println("Confirmar senha: ");
-            int confirmarSenha = keyboard.nextInt();
-            if (senhaCadastro == confirmarSenha) {
-                Usuario.cadastrar(senhaCadastro);
-            } else {
-                System.out.println("As senhas não são iguais!");
-                Logar();
+            System.out.println("------------Cadastro------------");
+
+            System.out.println("Nome: ");
+            String nome = keyboard.next();
+
+            System.out.println("Email: ");
+            String email = keyboard.next();
+
+            System.out.println("Confirmar email: ");
+            String confirmarEmail = keyboard.next();
+
+            if (Objects.equals(email, confirmarEmail)) {
+
+                System.out.println("""
+                        Gênero:\s
+                        M - Masculino
+                        F - Feminino""");
+                genero = keyboard.next().charAt(0);
+            }else {
+                System.out.println("Os emails não estão iguais!");
+               cadastrarEmail();
             }
-        } else if (opcao == 3) {
-            System.exit(0);
+                System.out.println("Cpf: ");
+                String cpf = keyboard.next();
+
+                System.out.println("Senha: ");
+                int senhaCadastro = keyboard.nextInt();
+                System.out.println("Confirmar senha: ");
+                int confirmarSenha = keyboard.nextInt();
+                if (senhaCadastro == confirmarSenha) {
+                    System.out.println("Digite a idade: ");
+                    int idade = keyboard.nextInt();
+                    if(idade >= 18){
+                        System.out.println("Numero do celular: ");
+                        int numeroCelular = keyboard.nextInt();
+
+                        Usuario usuario = new Usuario(senhaCadastro,nome,idade,cpf,numeroCelular,genero,email);
+                    }else{
+                        System.out.println("É necesserário ter 18 anos ou mais para registrar uma conta!");
+                    }
+                } else {
+                    System.out.println("As senhas não são iguais!");
+                    Logar();
+                }
+            } else if (opcao == 3) {
+                System.exit(0);
+            }
         }
+    public static void cadastrarEmail(){
+        System.out.println("Email: ");
+        String email = keyboard.next();
+        System.out.println("Confirmar email: ");
+        String confirmarEmail = keyboard.next();
+    }
+    public static void verificarCpf(String cpf){
+        char[] caracteres = new char[11];
+
+        for (int i = 1; i <= 8; i++) {
+            String caracter = SistemaLogin.substring(0,1);
+        }
+    }
+
+    private static String substring(int i, int i1) {
+        return null;
     }
 }
--- src/Login/Usuario.java
+++ src/Login/Usuario.java
@@ -3,18 +3,30 @@ package src.Login;
 import src.Loja.SystemShop;
 
 import java.util.Objects;
-
-import static src.Application.Main.operations;
-
 public class Usuario {
+
+    private static final int DDD = 13;
     private static int senha;
 
-    public Usuario(int senha) {
+    private String nome;
+    private int idade;
+    private String cpf;
+    private int numeroCelular;
+    private char genero;
+    private String email;
+
+
+    public Usuario(int senha, String nome, int idade, String cpf, int numeroCelular, char genero, String email) {
         this.senha = senha;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numeroCelular = numeroCelular;
+        this.genero = genero;
+        this.email = email;
     }
 
-    public static void cadastrar(int senhaCadastro) {
-        Usuario usuario = new Usuario(senhaCadastro);
+    public static void cadastrar(int senhaCadastro, String nome, int idade, String cpf, int numeroCelular, char genero, String email) {
+        Usuario usuario = new Usuario(senhaCadastro, nome, idade, cpf, numeroCelular, genero, email);
         System.out.println("Cadastro realizado com sucesso!");
         SistemaLogin.Logar();
     }
